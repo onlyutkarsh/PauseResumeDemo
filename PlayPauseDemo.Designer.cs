7d99379dadba6415e8a0c0b3ec275cdde2769596
Utkarsh <iamutkarsh@gmail.com>
2013-02-20 21:41:44 +0530

    Initial check-in
    Initial check-in of the demo app.

---
+++ PlayPauseDemo.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PauseResumeDemo
+{
+    partial class PlayPauseDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(10, 83);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(175, 83);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(336, 83);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(10, 13);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(401, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 3;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(10, 46);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(35, 13);
+            this.lblProgress.TabIndex = 4;
+            this.lblProgress.Text = "label1";
+            // 
+            // PlayPauseDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 118);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PlayPauseDemo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Play Pause Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ PlayPauseDemo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PauseResumeDemo
+{
+    public partial class PlayPauseDemo : Form
+    {
+        private BackgroundWorker _worker;
+        readonly ManualResetEvent _busy = new ManualResetEvent(false);
+
+        public PlayPauseDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (btnPause.Text.ToLower() == "pause")
+            {
+                PauseWorker();
+                btnPause.Text = "Resume";
+            }
+            else
+            {
+                StartWorker();
+                btnPause.Text = "Pause";
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnPause.Enabled = true;
+            _worker = new BackgroundWorker();
+            _worker.WorkerSupportsCancellation = true;
+            _worker.WorkerReportsProgress = true;
+            _worker.DoWork += DoWork;
+            _worker.ProgressChanged += ProgressChanged;
+            _worker.RunWorkerCompleted += WorkCompleted;
+            StartWorker();
+        }
+
+        private void WorkCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnPause.Enabled = false;
+            btnPause.Text = "Pause";
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+
+            lblProgress.Text = string.Format("{0}% complete!", e.ProgressPercentage);
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _busy.WaitOne();
+                if (_worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                var progress = i;
+                _worker.ReportProgress(++progress);
+                Thread.Sleep(80);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblProgress.Text = "Idle...";
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+            btnPause.Text = "Pause";
+
+        }
+
+        void StartWorker()
+        {
+            if (!_worker.IsBusy)
+            {
+                _worker.RunWorkerAsync();
+            }
+            // Unblock the worker 
+            _busy.Set();
+            lblProgress.Text = "In Progress...";
+        }
+
+        void PauseWorker()
+        {
+            // Block the worker
+            _busy.Reset();
+            lblProgress.Text += " Paused...";
+        }
+
+        void CancelWorker()
+        {
+            if (_worker.IsBusy)
+            {
+                _worker.CancelAsync();
+                lblProgress.Text = "Idle...";
+                progressBar.Value = 0;
+
+                // Unblock worker so it can see that
+                _busy.Set();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            CancelWorker();
+        }
+    }
+}
